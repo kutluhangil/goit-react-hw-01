102719ca3f6c86d17bbc5683b14612b17e573d21
kutluhangil <kutluhangul@windowslive.com>
2026-01-11 14:41:18 +0300

    Update Live Site

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import "./App.css";
 import Profile from "./components/Profile/Profile";
 import FriendList from "./components/FriendList/FriendList";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import Statistics from "./components/Statistics/Statistics";
 
 import userData from "./userData.json";
 import friends from "./friends.json";
 import transactions from "./transactions.json";
-import data from "./data.json";
 
 function App() {
   return (
@@ -20,7 +18,6 @@ function App() {
         image={userData.avatar}
         stats={userData.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </>
